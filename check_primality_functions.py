e3d8a6c8e4c600bd2511a53ea32d3dca87bd9554
Adam Froehlich <adamcfro@gmail.com>
2022-06-28 09:25:18 -0400

    Removed superfluous code, added description to function

--- check_primality_functions.py
+++ check_primality_functions.py
@@ -1,19 +1,8 @@
-# def get_integer(help_text="Give me a number: "):
-#     return int(input(help_text))
-
-# age = get_integer("Tell me your age: ")
-# school_year = get_integer("What grade are you in?: ")
-# if age > 15:
-#     print("You are over 15.")
-# print(f"You are in grade {school_year}.")
-
-
-
-
-#################
-
-
 def get_number():
+    """
+    This function takes in a number and returns a string stating whether or not
+    the number is prime.
+    """
     number = int(input("Give me a number: "))
     for num in range(2, number - 1):
         if number % num != 0:
